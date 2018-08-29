de0ab3d368ac84d1d3c7ab81e13a08b40de0ead2
Bloodblack <kaimaimoo123@gmail.com>
2018-08-29 14:23:25 +0700

    add set instance A , B

--- src/main/java/ct/af/message/incoming/parser/Parser_IDLE_Xxx.java
+++ src/main/java/ct/af/message/incoming/parser/Parser_IDLE_Xxx.java
@@ -1,5 +1,6 @@
 package ct.af.message.incoming.parser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.google.gson.Gson;
@@ -18,14 +19,29 @@ public class Parser_IDLE_Xxx {
         Param_IDLE_Xxx param = new Param_IDLE_Xxx();
         
 //        String rawDataMessage = eqxRawData.getRawDataAttribute("val");
-        String rawDataMessage = "{ \"A\" : \"a\" }";
+        String rawDataMessage = "{ \"A\" : \"a\","
+        		+ "\"B\" : [\"b1\",\"b2\"] }";
         String rawCType = eqxRawData.getCType();
         
         if(rawCType.equals("text/plain")) {
         	JsonParser jsonParser = new JsonParser();
     		JsonObject resourceOrderJsonObject = jsonParser.parse(rawDataMessage).getAsJsonObject();
     		Gson gson = GsonPool.getGson();
-    		HashMap<String, String> test = gson.fromJson(resourceOrderJsonObject, HashMap.class);
+			HashMap<String, Object> resourceHashMap = gson.fromJson(resourceOrderJsonObject, HashMap.class);
+    		
+    		if(resourceHashMap.containsKey("A")) {
+    			param.setA(resourceHashMap.get("A").toString());
+    		}
+    		if(resourceHashMap.containsKey("B")) {
+    			param.setB((ArrayList<String>) resourceHashMap.get("B"));
+    		}
+    		if(resourceHashMap.containsKey("C")) {
+    			
+    		}
+    		if(resourceHashMap.containsKey("D")) {
+    			
+    		}
+    		System.out.println("Helo");
         } else if(rawCType.equals("text/xml")) {
         	
         } else if(rawCType.equals("Diameter")) {
