f69fdcde8647fadb359bccaa3a0945feb3bd7568
Nutchapol <nutchapol.thathaii@gmail.com>
2018-09-03 10:27:24 +0700

    no message

--- src/main/java/ct/af/message/incoming/parser/Parser_IDLE_Xxx.java
+++ src/main/java/ct/af/message/incoming/parser/Parser_IDLE_Xxx.java
@@ -1,7 +1,18 @@
 package ct.af.message.incoming.parser;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
@@ -113,6 +124,41 @@ public class Parser_IDLE_Xxx {
             paramIdle.setCompany(company); 
             paramIdle.setCompany(company); 
             
+            DocumentBuilderFactory factory  = DocumentBuilderFactory.newInstance();
+           try {
+			DocumentBuilder builder = factory.newDocumentBuilder();
+			Document doc = builder.parse("paser.xml");
+			NodeList personList = doc.getElementsByTagName("person");
+			for(int i=0; i<personList.getLength();i++){
+				Node p = personList.item(i);
+				if(p.getNodeType() ==Node.ELEMENT_NODE){
+					Element person =  (Element) p ;
+					String id = person.getAttribute("id");
+					NodeList nameList = person.getChildNodes();
+					
+					
+					for(int j=0; j<nameList.getLength();j++){
+						Node n = nameList.item(j);
+						if(n.getNodeType() ==Node.ELEMENT_NODE){
+							Element names = (Element) n;
+							System.out.println("person " + id + ":" + names.getTagName()+ "=" + names.getTextContent());
+						}
+				}
+			}
+			
+			
+			}
+			
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
             
 
             
